6895ba75340cb79aad1f71a43e8a2087ff94d21b
Emmanuel Jaygbay <Emmanuel Jaygbay>
2020-08-27 17:41:59 +0000

    Functionalities added to calculator.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
-let operators = ['+', '-', '*', '/', '.'];
+let operators = ['+', '-', '*', '/', '.', '%'];
 
 const displayValue = (event) => {
-    document.getElementById('screen').value += event.target.value;
+    document.getElementById('screen').value.length === 1 && event.target.value === '0' ? document.getElementById('screen').value = "0" : document.getElementById('screen').value += event.target.value;
 }
 
 const getOperator = (event) => {
@@ -11,6 +11,7 @@ const getOperator = (event) => {
 const checkLastValue = (value) => {
     let value_displayed = document.getElementById('screen').value;
     operators.includes(value_displayed[value_displayed.length - 1]) ? console.log("Last value is an operator") : document.getElementById('screen').value += value;
+    value_displayed.length <= 0 && (value === '*' || value === '/' || value === '+' || value === '%') ? document.getElementById('screen').value = "" : "";
 }
 
 const clearButton = () => {
@@ -27,5 +28,6 @@ const deleteButton = () => {
 
 const equalButton = () => {
     let equal_value = eval(document.getElementById('screen').value);
-    equal_value === Infinity ? document.getElementById('screen').value = "Error" : document.getElementById('screen').value = equal_value;
+
+    equal_value === Infinity || isNaN(equal_value) ? document.getElementById('screen').value = "Error" : equal_value === undefined ? document.getElementById('screen').value = "" : document.getElementById('screen').value = equal_value;
 }
